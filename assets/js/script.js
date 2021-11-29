9d64b16df2ee0ce96c388f274be450c91eda9a69
Raymond Spartz <raymond.luke.spartz@gmail.com>
2021-11-29 12:36:18 -0600

    add ability to add task to li

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+
+// allow functionality of a button
+var buttonEl = document.querySelector("#save-task");
+console.log(buttonEl);
+
+
+//add to the list
+var buttonEl = document.querySelector("#save-task");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+// function stored as variable to handle button interactivity
+var createTaskHandler = function() {
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task.";
+    tasksToDoEl.appendChild(listItemEl);
+}
+
+// call createTaskHandler function
+buttonEl.addEventListener("click", createTaskHandler);
